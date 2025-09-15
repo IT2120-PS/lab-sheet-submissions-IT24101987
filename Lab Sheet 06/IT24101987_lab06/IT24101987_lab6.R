ed6796d4dda4a6b8ef602d7be9b148e4551c511f
IT24101987 <it24101987@my.sliit.lk>
2025-09-15 21:03:07 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24101987_lab06/IT24101987_lab6.R
@@ -0,0 +1,27 @@
+setwd("C:\\Users\\USER\\OneDrive\\Desktop\\IT24101987")
+#Exercise 2
+
+
+
+#Question 1
+#1
+#Binomial Distribution
+#n = 50 and p=0.85
+
+# 2
+
+dbinom(47,50,0.85)
+
+#Question 2
+
+# 1
+
+#  X is the number of customer calls received in one hour.
+
+#2
+
+# X has poisson distribution with lamda = 12
+
+# 3
+#What is the probability that exactly 15 calls are received in an hour?
+dpois(15,12)
